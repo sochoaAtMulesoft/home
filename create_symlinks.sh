620e789124f52ab55100fb3ce56529f2110e0d70
Sean Ochoa <sean.m.ochoa@gmail.com>
2013-05-08 10:37:15 -0700

    Updated various things.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -15,26 +15,6 @@ if [[ -f "$HOME/.pgpass" ]]; then
     chmod 0600 "$HOME/.pgpass"
 fi
 
-# update the gid/uid for all home files, preferring
-# the group id based on user name instead of the user's
-# primary group id based on the id command
-gid_pri="$(grep -Ei ^$USER /etc/group | grep -Eo '[0-9]+')"
-gid_sec="$(id | ack -o 'gid=\d+' | tr -d 'gid=')"
-gid=$gid_pri
-if [ "$gid" == "" ]; then
-    echo "Primary GID unavailable.  Using secondary GID."
-    gid=$gid_sec
-fi;
-uid=$(id | ack -o "uid=\d+" | tr -d 'uid=')
-echo "sudo chown $uid:$gid -R $HOME/.ssh"
-sudo chown $uid:$gid -R $HOME/.ssh 
-rc=$?
-
-if [ $rc -ne 0 ]; then 
-    echo "Changing ownership of $HOME/.ssh failed (rc = $?)."
-    exit 2
-fi
-
 chmod 0700 $HOME
 chmod 0700 $HOME/.ssh
 chmod 0600 $HOME/.ssh/authorized_keys
