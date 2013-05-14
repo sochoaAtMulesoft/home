e899374899cf97234beb26daa50af62acde31e95
Sean Ochoa <sean.m.ochoa@gmail.com>
2013-05-14 12:41:06 -0700

    Update to add a few things.

--- scripts/install_vim.sh
+++ scripts/install_vim.sh
@@ -11,6 +11,14 @@ cd $HOME/opt/tmp
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
 tar -jxf vim-7.3.tar.bz2
 cd vim73/
-./configure --prefix=$HOME/opt --enable-pythoninterp
+./configure --prefix=$HOME/opt \
+            --enable-pythoninterp \
+            --enable-gui=no
+            --without-x \
+            --enable-multibyte \
+            --with-tlib=ncurses \
+            --enable-rubyinterp \
+            --with-ruby-command=/usr/bin/ruby \
+            --with-features=huge
 make
 make install
